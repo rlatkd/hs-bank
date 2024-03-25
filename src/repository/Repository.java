28018affdb929fa0530b8d78cccb39f303868c98
dtd1614 <dtd1614@gmail.com>
2024-03-25 19:06:10 +0900

    refactor/auto increment 수정

--- src/repository/Repository.java
+++ src/repository/Repository.java
@@ -72,7 +72,7 @@ public abstract class Repository<E extends Entity> {
     }
     public final void add(E entity) throws BaseException {
         load();
-        entity.setId(getLastEntity() == null ? 0 : getLastEntity().getId());
+        entity.setId(getLastEntity() == null ? 0 : getLastEntity().getId() + 1);
         entityList.add(entity);
         save();
     }
